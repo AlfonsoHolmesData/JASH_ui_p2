46b47ad373b8266719c4c4661bdd23283cb3ff9d
Sean Dunn <60554213+spdunn@users.noreply.github.com>
2021-10-04 00:11:54 -0400

    new firebase who dis

--- src/util/Firebase.jsx
+++ src/util/Firebase.jsx
@@ -6,11 +6,12 @@ const firebaseConfig = {
     // seanprdunn@gmail.com
     apiKey: "AIzaSyBum_fcLp106Z-_7fL6ICh8Osxhx6V1vFA",
     authDomain: "revature-p2-4ae87.firebaseapp.com",
+    databaseURL: "https://revature-p2-4ae87-default-rtdb.firebaseio.com",
     projectId: "revature-p2-4ae87",
     storageBucket: "revature-p2-4ae87.appspot.com",
     messagingSenderId: "89348054115",
-    appId: "1:89348054115:web:da67dfc93463086953ec56",
-    measurementId: "G-KKDLBTE2HQ"
+    appId: "1:89348054115:web:86d3ceb0520ba1f353ec56",
+    measurementId: "G-R5HMKZXQC2"
     // proceedwithspeed@gmail.com
     // apiKey: "AIzaSyB8pS-aNMCBDeAq3kNCusKo7tw4UcJJvGk",
     // authDomain: "revature-p2.firebaseapp.com",
